9e47f4b5d448195249d1c7e9e859708fb456e519
Gratitude <gratitudekabelo9@gmail.com>
2024-03-16 13:16:23 +0200

    Formattted dy

--- src/index.js
+++ src/index.js
@@ -58,7 +58,12 @@ function handleSubmit(event) {
 
   searchCity(searchInput.value);
 }
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
+  return days[date.getDay()];
+}
 function getForecastdata(city) {
   let apiKey = `t919a78cf1d84obb72b8a3dcb00e6c94`;
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -72,10 +77,11 @@ function displayForecast(response) {
 
   let forecastHTML = "";
 
-  response.data.daily.forEach(function (day) {
-    forecastHTML += `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHTML += `
       <div class="weather-forecast-day">
-        <div class="weather-forecast-date">Tue</div>
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
         <div class="weather-forecast-icon"><img src="${
           day.condition.icon_url
         }"/></div>
@@ -88,6 +94,7 @@ function displayForecast(response) {
           )}°</div>
         </div>
       </div>`;
+    }
   });
 
   let forecastElement = document.querySelector("#forecast");
